4c2b3801132eeff610fc0f5d6fa5040430b74ba2
unknown <andrelopessfla@gmail.com>
2020-05-08 13:22:33 -0300

    criado o contrutor, o state e um metodo para converter a moeda

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Conversor from './components/Conversor';
 
 function App() {
   return (
     <div >
-      
-    </div>
+
+      <Conversor moedaA = "USD" moedaB="BRL"> </Conversor>
+
+    </div>  
   );
 }
 
